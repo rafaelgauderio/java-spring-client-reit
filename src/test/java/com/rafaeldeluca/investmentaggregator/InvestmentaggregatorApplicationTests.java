7a8d5d3b86794b766d7b2ea40b085d0447f9ec21
Rafael de Luca <deluca1712@gmail.com>
2026-04-15 16:06:37 -0300

    Create project

---
+++ src/test/java/com/rafaeldeluca/investmentaggregator/InvestmentaggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.investmentaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentaggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
